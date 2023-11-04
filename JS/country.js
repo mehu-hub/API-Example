4f361d47ccb2bb91d384b2843acb5d0cccba1abe
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-05 04:46:35 +0600

    Update

--- JS/country.js
+++ JS/country.js
@@ -9,15 +9,34 @@ const displayCountry = countries => {
 
 
     countries.forEach(country => {
-        console.log(country)
+        // console.log(country)
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
          <h1>Name: ${country.name.common}</h1> 
          <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+         <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `; 
         countryContainer.appendChild(countryDiv);
     })
+
+}
+
+const loadCountryDetails = code =>{
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryData(data[0]))
+}
+
+const showCountryData = country =>{
+    const countryDetails = document.getElementById('country-detail')
+
+    countryDetails.innerHTML = `
+    <h1>${country.name.common}</h1>
+    <img src="${country.flags.png}">
+    `;
+
 
 }
 
