f36f85b9640b2202822ab048adabec9f98724d30
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-06 05:08:56 +0600

    Added Modal Info

--- JS/food.js
+++ JS/food.js
@@ -16,8 +16,13 @@ const displayMeals = meals => {
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
-                    <p class="card-text">${meal.strInstructions}</p>
+                    <p class="card-text"></p>
                 </div>
+                <div class="p-2">
+                    <button onclick="loadMealDetails(${meal.idMeal})" type="button" class="btn w-50 btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                        Meal Details
+                    </button> 
+                </div>  
             </div>
         `
 
@@ -25,11 +30,35 @@ const displayMeals = meals => {
     })
 }
 
-const searchMeals = () =>{
+const searchMeals = () => {
     const searchText = document.getElementById('search-field').value;
     loadData(searchText)
-    
+
 }
 
 
-loadData()
+const loadMealDetails = idMeal =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]))
+}
+
+const displayMealDetails = meal =>{
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealDetails = document.getElementById('mealDetailsBody');
+    mealDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}">
+        <ul>
+            <li>${meal.strIngredient1}</li>
+            <li>${meal.strIngredient2}</li>
+            <li>${meal.strIngredient3}</li>
+            <li>${meal.strIngredient4}</li>
+            <li>${meal.strIngredient5}</li> 
+        </ul>
+        <p>${meal.strInstructions}</p>
+        
+    `
+}
+
+loadData('chicken')
