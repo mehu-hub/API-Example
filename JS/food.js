15f4fc33c398e9d899da14318039dd0a3a523965
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-06 04:16:05 +0600

    Create Card

--- JS/food.js
+++ JS/food.js
@@ -1,7 +1,26 @@
-const loadData = () => { 
+const loadData = () => {
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
         .then(res => res.json())
-        .then(data => console.log(data.meals)) 
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    const mealsContainer = document.getElementById('meals-container');
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+            <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `
+
+        mealsContainer.appendChild(mealDiv);
+    })
 }
 
 loadData()
